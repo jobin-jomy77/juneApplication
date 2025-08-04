204258a9aa6d287f99273f31105181db7d76e45c
Jobin Jomy Mathew <jerryjoben@gmail.com>
2025-08-04 23:44:25 +0530

    new

--- juneapp/lib/FirstScreen.dart
+++ juneapp/lib/FirstScreen.dart
@@ -12,6 +12,7 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
@@ -28,19 +29,25 @@ class _FirstScreenState extends State<FirstScreen> {
           ),
 
 
-          Text("Welcome to our App",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.bold
-            ),),
+          Padding(
+            padding: const EdgeInsets.only(left: 25,top: 5),
+            child: Text("Welcome to our App",
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+              ),),
+          ),
 
-          Text("This is the First Screen of our App",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.bold
-            ),)
+          Padding(
+            padding: const EdgeInsets.only(left: 25,top: 5),
+            child: Text("This is the First Screen of our App",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+              ),),
+          )
         ],
       ),
     );
